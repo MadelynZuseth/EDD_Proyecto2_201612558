f46d2769e1f4719957ea9194e1fb8612031cc8c6
elivar <madelyn.perez07@hotmail.com>
2020-11-02 04:01:31 -0600

    segundo

---
+++ src/safeway/Page.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package safeway;
+
+/**
+ *
+ * @author MADELYN
+ */
+class Page {
+    
+    private int max;
+    private Page paginaPadre;
+    private Nodo[] llaves;
+
+    
+    /**
+     * @return the max
+     */
+    
+    public void put(int i, Nodo llave){
+        this.getLlaves()[i]= llave;
+    }
+    
+    public Nodo get(int i){
+        return this.getLlaves()[i];
+    }
+
+    /**
+     * @return the max
+     */
+    public int getMax() {
+        return max;
+    }
+
+    /**
+     * @param max the max to set
+     */
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    /**
+     * @return the paginaPadre
+     */
+    public Page getPaginaPadre() {
+        return paginaPadre;
+    }
+
+    /**
+     * @param paginaPadre the paginaPadre to set
+     */
+    public void setPaginaPadre(Page paginaPadre) {
+        this.paginaPadre = paginaPadre;
+    }
+
+    /**
+     * @return the llaves
+     */
+    public Nodo[] getLlaves() {
+        return llaves;
+    }
+
+    /**
+     * @param llaves the llaves to set
+     */
+    public void setLlaves(Nodo[] llaves) {
+        this.llaves = llaves;
+    }
+}
+   
